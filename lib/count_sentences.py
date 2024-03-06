1cbfd7911413a5218905623f6d24cde0c7a31b28
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2024-03-06 15:32:36 +0300

    Complered assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value = ""):
+      self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+        else:
+            self._value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+    
+    def is_question(self):
+        return self.value.endswith('?')
+    
+    def is_exclamation(self):
+        return self.value.endswith('!')
+    
+    def count_sentences(self):
+        sentence_end_punctuation = ['.', '!', '?']
+        sentence_count = sum(1 for i, char in enumerate(self.value) if char in sentence_end_punctuation and (i == len(self.value) - 1 or self.value[i+1] not in sentence_end_punctuation))
+        return sentence_count
